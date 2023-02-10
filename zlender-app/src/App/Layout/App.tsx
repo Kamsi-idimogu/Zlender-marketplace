74ad4525f880f68baafdc0b5b34dc48df0044784
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-02-10 14:14:13 -0500

    Add product.ts class

---
+++ zlender-app/src/App/Layout/App.tsx
@@ -0,0 +1,11 @@
+import React, { useState } from 'react';
+import { Product } from '../Models/product';
+
+function App() {
+  const [product, setProduct] = useState<Product>()
+  return (
+    <div ></div>
+  );
+}
+
+export default App;
--- zlender-app/src/index.tsx
+++ zlender-app/src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
+import './App/Layout/styles.scss';
+import App from './App/Layout/App';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
