f9759cf5f3ae7c782c6778e108b1c84f68b4cc9f
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-02-23 00:48:59 -0500

    create context class for states

--- zlender-app/src/index.tsx
+++ zlender-app/src/index.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { RouterProvider } from 'react-router-dom';
+import { StoreProvider } from './App/Context/StoreContext';
 // import style from './App/Layout/styles.scss';
 import { router } from './App/Router/Routes';
 
@@ -9,6 +10,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <StoreProvider>
+      <RouterProvider router={router} />
+    </StoreProvider>
   </React.StrictMode>
 );
