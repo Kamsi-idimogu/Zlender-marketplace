c26ea349ebf46f92787a211f54cbdd3f89b4b8a1
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-02-23 01:06:14 -0500

    create cookie utility class

--- zlender-app/src/App/Layout/App.tsx
+++ zlender-app/src/App/Layout/App.tsx
@@ -1,12 +1,28 @@
 import { createTheme, CssBaseline, ThemeProvider } from '@mui/material';
 import { Container } from '@mui/system';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 import Header from './Header';
 import 'react-toastify/dist/ReactToastify.css';
+import { UseStoreContext } from '../Context/StoreContext';
+import agent from '../Api/agent';
+import LoadingComponent from './LoadingComponent';
+import { getCookies } from '../Util/util';
 
 function App() {
+  const {setBasket} = UseStoreContext();
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const buyerId = getCookies('buyerId');
+    if(buyerId){
+      agent.Basket.get()
+        .then(basket => setBasket(basket))
+        .finally(() => setLoading(false));
+    }
+  }, [setBasket])
+
   const [darkMode, setDarkMode] = useState(false);
 
   const paletteType = darkMode ? 'dark' : 'light';
@@ -23,6 +39,8 @@ function App() {
   function handleThemeChange(){
     setDarkMode(!darkMode);
   }
+
+  if(loading) return <LoadingComponent message='Initializing App...'/>
 
   return (
     <ThemeProvider theme={theme}>
