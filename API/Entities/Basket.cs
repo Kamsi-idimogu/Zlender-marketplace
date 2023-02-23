c2d7342f4ee4faa63228303bfbe7836a5b296a19
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-02-22 22:23:50 -0500

    create basket class and migration in api

---
+++ API/Entities/Basket.cs
@@ -0,0 +1,33 @@
+using System.Collections.Generic;
+using System.Linq;
+
+namespace API.Entities
+{
+    public class Basket
+    {
+        public int Id { get; set; }
+
+        public string BuyerId { get; set; }
+
+        public List<BasketItem> Items { get; set; } = new(); //initialize it so it won't be null (it's an empty list)
+
+        public void AddItem(Product product, int quantity)
+        {
+            if(Items.All(item => item.ProductId != product.Id)) //if the product has not been added before
+            {
+                Items.Add(new BasketItem{Product = product, Quantity = quantity});
+            }
+
+            var existingItem = Items.FirstOrDefault(item => item.ProductId == product.Id);
+            if(existingItem != null) existingItem.Quantity += quantity;
+        }
+
+        public void RemoveItem(int productId, int quantity)
+        {
+            var item = Items.FirstOrDefault(item => item.ProductId == productId);
+            if(item == null) return;
+            item.Quantity -= quantity;
+            if(item.Quantity == 0) Items.Remove(item);
+        }
+    }
+}
