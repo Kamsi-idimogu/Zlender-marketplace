43c15b9dc792d4159d17dcf731e0189a680d49fb
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-03-06 11:05:58 -0500

    Set up identity package and migrations

--- API/Data/StoreContext.cs
+++ API/Data/StoreContext.cs
@@ -1,9 +1,11 @@
 using API.Entities;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
 namespace API.Data
 {
-    public class StoreContext : DbContext //StoreContext derived from DbContext (extends)
+    public class StoreContext : IdentityDbContext<User> //StoreContext derived from IdentityDbContext (extends)
     {
         public StoreContext(DbContextOptions options) : base(options)
         {
@@ -12,5 +14,16 @@ namespace API.Data
         public DbSet<Product> Products { get; set; }
 
         public DbSet<Basket> Baskets { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.Entity<IdentityRole>()
+                .HasData(
+                    new IdentityRole{Name = "Member", NormalizedName = "MEMBER"},
+                    new IdentityRole{Name = "Admin", NormalizedName = "ADMIN"}
+                );
+        }
     }
 }
