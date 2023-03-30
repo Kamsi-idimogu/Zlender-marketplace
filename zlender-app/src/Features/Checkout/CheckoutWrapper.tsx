0fb9139e636ee57a3d1782f6e4684c582ef27108
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-03-30 17:21:21 -0400

    created payments endpoint in frontend

--- zlender-app/src/Features/Checkout/CheckoutWrapper.tsx
+++ zlender-app/src/Features/Checkout/CheckoutWrapper.tsx
@@ -1,10 +1,27 @@
 import { Elements } from "@stripe/react-stripe-js";
 import { loadStripe } from "@stripe/stripe-js";
+import { useEffect, useState } from "react";
+import agent from "../../App/Api/agent";
+import LoadingComponent from "../../App/Layout/LoadingComponent";
+import { useAppDispatch } from "../../App/Store/configureStore";
+import { setBasket } from "../Basket/basketSlice";
 import CheckoutPage from "./CheckoutPage";
 
 const stripePromise = loadStripe('pk_test_51MrQJzDq8wAGEaQAwvrFHexehnTZ5wgqIbWrDwaiI9gJFimqPtkqvmCryjt9AGWpYVeMtGwKMMH5f5rVLCxmeGSt00nwkKirBS')
 
 export default function CheckoutWrapper() {
+    const dispatch = useAppDispatch();
+    const [loading, setLoading] = useState(true);
+
+    useEffect(() => {
+        agent.Payments.createPaymentIntent()
+            .then(basket => dispatch(setBasket(basket)))
+            .catch(error => console.log(error))
+            .finally(() => setLoading(false))
+    }, [dispatch]);
+
+    if (loading) return <LoadingComponent message="Loading checkout..."/>
+
     return (
         <Elements stripe={stripePromise}>
             <CheckoutPage/>
