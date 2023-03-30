76367c471f918bf944aafe6f08f463a9649b5049
Kamsi-idimogu <90581167+Kamsi-idimogu@users.noreply.github.com>
2023-03-30 17:54:52 -0400

    Added submit payment function

--- zlender-app/src/Features/Checkout/CheckoutPage.tsx
+++ zlender-app/src/Features/Checkout/CheckoutPage.tsx
@@ -7,10 +7,11 @@ import Review from "./Review";
 import { yupResolver } from '@hookform/resolvers/yup';
 import { validationSchema } from "./checkoutValidation";
 import agent from "../../App/Api/agent";
-import { useAppDispatch } from "../../App/Store/configureStore";
+import { useAppDispatch, useAppSelector } from "../../App/Store/configureStore";
 import { clearBasket } from "../Basket/basketSlice";
 import { LoadingButton } from "@mui/lab";
 import { StripeElementType } from "@stripe/stripe-js";
+import { CardNumberElement, useElements, useStripe } from "@stripe/react-stripe-js";
 
 const steps = ['Shipping address', 'Review your order', 'Payment details'];
 
@@ -22,7 +23,12 @@ export default function CheckoutPage() {
 
     const [cardState, setCardState] = useState<{elementError: {[key in StripeElementType]?: string}}>({elementError: {}});
     const [cardComplete, setCardComplete] = useState<any>({cardNumber: false, cardExpiry: false, cardCvc: false});
-  
+    const [paymentMessage, setPaymentMessage] = useState('');
+    const [paymentSucceeded, setPaymentSucceeded] = useState(false);
+    const {basket} = useAppSelector(state => state.basket);
+    const stripe = useStripe();
+    const elements  = useElements();
+
     function onCardInputChange(event: any){
       setCardState({
         ...cardState,
@@ -62,20 +68,44 @@ export default function CheckoutPage() {
             })
     }, [methods])
 
-    const handleNext = async (data: FieldValues) => {
+    async function submitOrder(data: FieldValues) {
+        setLoading(true);
         const {nameOnCard, saveAddress, ...shippingAddress} = data;
-        if(activeStep === steps.length - 1){
-            setLoading(true);
-            try {
+        if (!stripe || !elements) return; //stripe is not ready
+        try {
+            const cardElement = elements.getElement(CardNumberElement);
+            const paymentResult = await stripe.confirmCardPayment(basket?.clientSecret!, {
+                payment_method: {
+                    card: cardElement!,
+                    billing_details: { //could add additional options like address details
+                        name: nameOnCard
+                    }
+                }
+            });
+            console.log(paymentResult);
+            if (paymentResult.paymentIntent?.status === 'succeeded'){
                 const orderNumber = await agent.Orders.create({saveAddress, shippingAddress});
                 setOrderNumber(orderNumber);
+                setPaymentSucceeded(true)
+                setPaymentMessage('Thank you - we have receieved your payment')
                 setActiveStep(activeStep + 1);
                 dispatch(clearBasket());
                 setLoading(false);
-            } catch(error) {
-                console.log(error);
+            } else {
+                setPaymentMessage(paymentResult.error?.message!);
+                setPaymentSucceeded(false);
                 setLoading(false);
+                setActiveStep(activeStep + 1);
             }
+        } catch (error) {
+            console.log(error)
+            setLoading(false)
+        }
+    }
+
+    const handleNext = async (data: FieldValues) => {
+        if(activeStep === steps.length - 1){
+            await submitOrder(data);
         }
         else {
             setActiveStep(activeStep + 1);
@@ -114,13 +144,20 @@ export default function CheckoutPage() {
                     {activeStep === steps.length ? (
                         <>
                             <Typography variant="h5" gutterBottom>
-                                Thank you for your order.
+                                {paymentMessage}
                             </Typography>
-                            <Typography variant="subtitle1">
+                            {paymentSucceeded ? (
+                                <Typography variant="subtitle1">
                                 Your order number is #{orderNumber}. We have not emailed your order
                                 confirmation, and will not send you an update when your order has
                                 shipped as this is a demo store.
-                            </Typography>
+                                </Typography>
+                            ) : (
+                                <Button variant="contained" onClick={handleBack}>
+                                    Go back and try again
+                                </Button>
+                            )}
+                            
                         </>
                     ) : (
                         <form onSubmit={methods.handleSubmit(handleNext)}>
